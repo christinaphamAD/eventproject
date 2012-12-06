47ce1b946c87062034c39d27e29e92a7a1ce01f1
Christina Pham <Christina@Christinas-MacBook-Pro.local>
2012-12-06 11:27:19 -0800

    Fixed some UI stuff with the Search Bar. Included a separate js file for location.

---
+++ js/locations.js
@@ -0,0 +1,7 @@
+var sites = [
+	['Mount Evans', 39.58108, -105.63535, 4, 'This is Mount Evans.'],
+	['Irving Homestead', 40.315939, -105.440630, 2, 'This is the Irving Homestead.'],
+	['Badlands National Park', 43.785890, -101.90175, 1, 'This is Badlands National Park'],
+	['Flatirons in the Spring', 39.99948, -105.28370, 3, 'These are the Flatirons in the spring.']
+	
+];
